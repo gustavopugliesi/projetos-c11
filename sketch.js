d770a205cb9a1d89a10391c6eab178fad9a515f5
gustavopugliesi <93950032+gustavopugliesi@users.noreply.github.com>
2021-12-06 18:16:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+var jaxon
+
+function setup(){
+  createCanvas(400,400);
+  jaxon = createSprite (200,250, 20, 20);
+  jaxon.AddImage("Runner-1.png")
+}
+
+function draw() 
+{
+  background(0);
+
+  if (keyIdDown(a) {
+
+    jaxon.AddImage("Runner-1.png")
+    jaxon.velocityX = 2
+  }
+
+  if (keyIdDown(d) {
+
+    jaxon.AddImage("Runner-2.png")
+    jaxon.velocityX = -2
+  }
+}
